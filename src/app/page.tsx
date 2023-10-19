6e0ce3a1cfa950d3e2b7e5995f580fe6e4508f7e
chaipat supharak <chaipat.sup@cpf.co.th>
2023-10-19 12:36:51 +0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,7 @@
 import Config from '../../config/config'
 
 export default function Home() {
-  console.log(JSON.stringify(Config.NEXT_PUBLIC_ENV));
-  console.log(JSON.stringify("CC"))
-  return (
-    <div>
-      NEXT_PUBLIC_ENV = {Config.NEXT_PUBLIC_ENV}
-    </div>
-  )
+  console.log(JSON.stringify(Config.NEXT_PUBLIC_ENV))
+  console.log(JSON.stringify('CC'))
+  return <div>NEXT_PUBLIC_ENV = {Config.NEXT_PUBLIC_ENV}</div>
 }
